bb8065235c88cf5d2d54f59501b29ed1333690f6
Chronosamoht <chronosamoht@gmail.com>
2014-11-07 15:18:16 +0000

    Refonte du squelette de la page d'acceuil

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<?php include 'header.html' ?>
+
+
+
+
+
+
+
+
+<main>
+<!-- Partie principale de la page -->
+
+<h2> Organisez vos vacances en 3 étapes </h2>
+
+
+<section>
+<!-- Rechercher Sélectionner Plannifier -->
+
+
+
+
+<section>
+<!-- populaire -->
+
+
+</section>
+<section>
+<!-- News -->
+
+
+</section>
+<section>
+<!-- Promotions -->
+
+
+</section>
+
+
+</section>
+
+
+</main>
+
+<?php include 'footer.html' ?>
