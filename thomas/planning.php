ab9902c511d05a8b229331f4422a0d12d8111200
Chronosamoht <chronosamoht@gmail.com>
2014-11-10 18:03:38 +0000

    je créer les autres fichers

---
+++ thomas/planning.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+  <title>Travel Voyage !</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+<?php include("base/header.php"); ?>
+
+<h1> Travel voyage : le meilleur de vos vacances ! </h1>
+<?php include("base/menu.php"); ?>
+<main class="planning">
+
+<h2> Mon Planning </h2>
+
+
+
+
+</main>
+
+<?php include("base/footer.php"); ?>
+
+
+</body>
+
+
+</html>
