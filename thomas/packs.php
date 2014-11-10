a3747ec455925fce8daad826613ccb0446808819
Chronosamoht <chronosamoht@gmail.com>
2014-11-10 15:08:29 +0000

    j'ajoute les packs

---
+++ thomas/packs.php
@@ -0,0 +1,122 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+  <title>Travel Voyage !</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<?php include("header.php"); ?>
+
+<h1> Travel voyage : le meilleur de vos vacances ! </h1>
+<?php include("menu.php"); ?>
+<main>
+<!-- Partie principale de la page -->
+
+<h2> Les Packs ! </h2>
+
+
+<div>
+
+<section class="Packs"> 
+<h3> Pack Détente  </h3>
+<p>
+Proin placerat placerat sagittis. Ut vel nisl luctus erat placerat sollicitudin vel a urna. 
+Nulla eget purus condimentum, imperdiet nulla viverra, hendrerit felis. 
+Fusce eu elit sit amet enim condimentum condimentum. Nunc et maximus leo. Nulla facilisi. 
+Nunc faucibus velit sit amet nisl lobortis, in ultricies mauris consequat...
+<br/>
+Prix : 800€  <!-- SELECT prix FROM pack WHERE num_pack= ; -->
+Durée : Une semaine <!-- SELECT dure FROM pack WHERE num_pack= ; -->
+</p>
+
+<div>
+<button> Plus d'info </button>
+<button> Ajouter au Panier  </button>
+
+</div>
+
+
+</section>
+
+<section class="Packs"> 
+<h3> Pack Sportif</h3>
+
+<p>
+Proin placerat placerat sagittis. Ut vel nisl luctus erat placerat sollicitudin vel a urna. 
+Nulla eget purus condimentum, imperdiet nulla viverra, hendrerit felis. 
+Fusce eu elit sit amet enim condimentum condimentum. Nunc et maximus leo. Nulla facilisi. 
+Nunc faucibus velit sit amet nisl lobortis, in ultricies mauris consequat...
+<br/>
+Prix : 800€  <!-- SELECT prix FROM pack WHERE num_pack= ; -->
+Durée : Une semaine <!-- SELECT dure FROM pack WHERE num_pack= ; -->
+</p>
+
+<div>
+<button> Plus d'info </button>
+<button> Ajouter au Panier  </button>
+
+</div>
+
+</section>
+
+
+</div>
+
+<div>
+
+<section class="Packs"> 
+<h3> Pack Famille </h3>
+<p>
+Proin placerat placerat sagittis. Ut vel nisl luctus erat placerat sollicitudin vel a urna. 
+Nulla eget purus condimentum, imperdiet nulla viverra, hendrerit felis. 
+Fusce eu elit sit amet enim condimentum condimentum. Nunc et maximus leo. Nulla facilisi. 
+Nunc faucibus velit sit amet nisl lobortis, in ultricies mauris consequat...
+<br/>
+Prix : 800€  <!-- SELECT prix FROM pack WHERE num_pack= ; -->
+Durée : Une semaine <!-- SELECT dure FROM pack WHERE num_pack= ; -->
+</p>
+
+<div>
+<button> Plus d'info </button>
+<button> Ajouter au Panier  </button>
+
+</div>
+
+</section>
+
+<section class="Packs"> 
+<h3> Packs Gastronomique</h3>
+
+<p>
+Proin placerat placerat sagittis. Ut vel nisl luctus erat placerat sollicitudin vel a urna. 
+Nulla eget purus condimentum, imperdiet nulla viverra, hendrerit felis. 
+Fusce eu elit sit amet enim condimentum condimentum. Nunc et maximus leo. Nulla facilisi. 
+Nunc faucibus velit sit amet nisl lobortis, in ultricies mauris consequat...
+<br/>
+Prix : 800€  <!-- SELECT prix FROM pack WHERE num_pack= ; -->
+Durée : Une semaine <!-- SELECT dure FROM pack WHERE num_pack= ; -->
+</p>
+
+<div>
+<button> Plus d'info </button>
+<button> Ajouter au Panier  </button>
+
+</div>
+</section>
+
+
+</div>
+
+</main>
+
+<?php include("footer.php"); ?>
+
+
+</body>
+
+
+</html>
