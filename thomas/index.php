fceeeff67ac6fc3c70160ed9399b5477d0697d5c
Chronosamoht <chronosamoht@gmail.com>
2014-11-11 16:39:17 +0000

    syncro

--- thomas/index.php
+++ thomas/index.php
@@ -1,15 +1,5 @@
 <!doctype html>
 
-<<<<<<< HEAD
-<!doctype html>
-
-<html lang="fr">
-<head>
-  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
-  <title>Travel Voyage !</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-=======
 <html lang="fr">
 <head>
   <meta content="text/html; charset=UTF-8" http-equiv="content-type">
@@ -24,97 +14,32 @@
 
 <h1> Travel voyage : le meilleur de vos vacances ! </h1>
 <?php include("menu.php"); ?>
+
 <main>
 <!-- Partie principale de la page -->
->>>>>>> 762383ae54a1fc508f084c8817529ee76c0fc3aa
-
-<body>
 
 
-<?php include("base/header.php"); ?>
-
-<h1> Travel voyage : le meilleur de vos vacances ! </h1>
-<?php include("base/menu.php"); ?>
-
-
-<<<<<<< HEAD
-=======
-<h2> Organisez vos vacances en 3 étapes : </h2>
->>>>>>> 762383ae54a1fc508f084c8817529ee76c0fc3aa
-
-
-
-<p>
-<h3> Rechercher </h3>
-</p>
-
-<p>
-<h3> Sélectionner </h3>
-</p>
-
-<<<<<<< HEAD
 <h2> Organisez vos vacances en 3 étapes : </h2>
 
 
 <section>
 <!-- Rechercher Sélectionner Plannifier -->
-=======
-<p>
-<h3> Plannifier </h3>
-</p>
-
-</section>
->>>>>>> 762383ae54a1fc508f084c8817529ee76c0fc3aa
 
 <p>
 <h3> Rechercher </h3>
 </p>
 
-<<<<<<< HEAD
 <p>
 <h3> Sélectionner </h3>
 </p>
 
 <p>
 <h3> Plannifier </h3>
-=======
-<section class="bonplan">
-<!-- Bon plan -->
-<h3> Bon plan </h3>
-
-<p>
-<!-- populaire -->
-Populaire
-<br/>
-<a  href="index.php?lang=fr">
-<!-- photo_vacances -->
-<img src="img/imgvacances/040.jpg" alt="populaire">
-</a>
-</p>
-
-<p>
-<!-- News -->
-News <br/>
-<a  href="index.php?lang=fr">
-<!-- photo_vacances -->
-<img src="img/imgvacances/047.jpg" alt="News">
-</a>
-</p>
-
-<p>
-<!-- Promotions -->
-Promotions <br/>
-<a  href="index.php?lang=fr">
-<!-- photo_vacances -->
-<img src="img/imgvacances/57.jpg" alt="Promotions">
-</a>
->>>>>>> 762383ae54a1fc508f084c8817529ee76c0fc3aa
 </p>
 
 </section>
 
 
-<<<<<<< HEAD
 <section class="bonplan">
 <!-- Bon plan -->
 <h3> Bon plan </h3>
@@ -146,17 +71,11 @@ Promotions <br/>
 <img src="img/imgvacances/57.jpg" alt="Promotions">
 </a>
 </p>
-=======
+
+</section>
+
+
 </main>
-
-<?php include("footer.php"); ?>
->>>>>>> 762383ae54a1fc508f084c8817529ee76c0fc3aa
-
-
-</body>
-
-
-<<<<<<< HEAD
 
 <?php include("base/footer.php"); ?>
 
