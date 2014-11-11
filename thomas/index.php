9200ed40fe3b08092c6896cd5c75cec8599f8c9b
Chronosamoht <chronosamoht@gmail.com>
2014-11-11 16:02:42 +0000

    recupération

--- thomas/index.php
+++ thomas/index.php
@@ -1,72 +1,92 @@
 
-<?php include 'header.html' ?>
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+  <title>Travel Voyage !</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
 
 
-
-<<<<<<< HEAD
-=======
 <?php include("base/header.php"); ?>
 
 <h1> Travel voyage : le meilleur de vos vacances ! </h1>
 <?php include("base/menu.php"); ?>
-<main>
-<!-- Partie principale de la page -->
->>>>>>> parent of 4296a18... sincronysation avec le serveur de nico
-
-
-
-
 
 <main>
 <!-- Partie principale de la page -->
 
-<h2> Organisez vos vacances en 3 étapes </h2>
+
+
+
+
+
+<h2> Organisez vos vacances en 3 étapes : </h2>
 
 
 <section>
 <!-- Rechercher Sélectionner Plannifier -->
 
-<<<<<<< HEAD
-
-
-=======
+<p>
 <h3> Rechercher </h3>
+</p>
+
+<p>
 <h3> Sélectionner </h3>
+</p>
+
+<p>
 <h3> Plannifier </h3>
+</p>
 
 </section>
->>>>>>> parent of 4296a18... sincronysation avec le serveur de nico
 
-<section>
-<!-- populaire -->
 
-<<<<<<< HEAD
-=======
 <section class="bonplan">
 <!-- Bon plan -->
-<h3> Bon plans </h3>
->>>>>>> parent of 4296a18... sincronysation avec le serveur de nico
+<h3> Bon plan </h3>
 
-</section>
-<section>
+<p>
+<!-- populaire -->
+Populaire
+<br/>
+<a  href="index.php?lang=fr">
+<!-- photo_vacances -->
+<img src="img/imgvacances/040.jpg" alt="populaire">
+</a>
+</p>
+
+<p>
 <!-- News -->
+News <br/>
+<a  href="index.php?lang=fr">
+<!-- photo_vacances -->
+<img src="img/imgvacances/047.jpg" alt="News">
+</a>
+</p>
 
-
-</section>
-<section>
+<p>
 <!-- Promotions -->
-
-
-</section>
-
-<<<<<<< HEAD
-=======
-<?php include("base/footer.php"); ?>
->>>>>>> parent of 4296a18... sincronysation avec le serveur de nico
+Promotions <br/>
+<a  href="index.php?lang=fr">
+<!-- photo_vacances -->
+<img src="img/imgvacances/57.jpg" alt="Promotions">
+</a>
+</p>
 
 </section>
 
 
 </main>
 
-<?php include 'footer.html' ?>
+
+<?php include("base/footer.php"); ?>
+
+
+</body>
+
+
+</html>
