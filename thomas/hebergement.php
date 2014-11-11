b6287e7b6815499c906dc778ee95ce56526a50d8
Chronosamoht <chronosamoht@gmail.com>
2014-11-11 12:17:12 +0000

    Revert "yay"
    This reverts commit 28eabb51023e6ff6062dd31b05cfd6883ce70842.

--- thomas/hebergement.php
+++ thomas/hebergement.php
@@ -18,43 +18,7 @@
 
 <h2> Les Hébergements </h2>
 
-<div>
 
-<p>
-<h3> Chalet </h3>
-<a  href="index.php?lang=fr">
-<img src="img/imgacti/chalet.jpg" alt="chalet">
-</a>
-
-</p>
-
-<p>
-<h3> Gîte </h3>
-<a  href="index.php?lang=fr">
-<img src="img/imgacti/gite1.jpg" alt="Gite">
-</a>
-
-</p>
-
-</div>
-
-<div>
-<p>
-<h3> Refuge </h3>
-<a  href="index.php?lang=fr">
-<img src="img/imgacti/refuge.jpg" alt="Refuge">
-</a>
-
-</p>
-
-<p>
-<h3> Hotel </h3>
-<a  href="index.php?lang=fr">
-<img src="img/imgacti/hotel.jpg" alt="Hotel">
-</a>
-
-</p>
-</div>
 
 
 </main>
